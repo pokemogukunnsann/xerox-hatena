e76b62cde93c5ac9c3fe192b7a61be16d15791b4
pokemogukunnsann <konnnitiha.sayounara@icloud.com>
2025-11-04 11:21:45 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.get('/api/video', async (req, res) => {
     // 関連動画取得（100件まで、深掘り）
     let relatedVideos = [];
     const MAX_VIDEOS = 10000;
-    console.log(MAX_VIDEOS(
+    console.log(MAX_VIDEOS)
 
     // 1. 初期の関連動画ソースを特定（複数のプロパティから優先順位に従って探す）
     let initialRelated = info.related || [];
